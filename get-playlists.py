5858d2a85ea27ede5793be3b429928784c0bfb95
Kevin <kevinecasey@gmail.com>
2017-03-14 14:52:00 -0500

    read password from the command line

--- get-playlists.py
+++ get-playlists.py
@@ -1,35 +1,40 @@
+import sys
 from gmusicapi import Mobileclient
 from datetime import date
 import fileinput
 
-for line in fileinput.input():
-    print(line)
-
 lists_to_get = ['Party Mix','Rap','Blues','Ivy music']
 
-api = Mobileclient()
-#TODO read pw from command line
-logged_in = api.login('kevinecasey@gmail.com','zehovhwxlowhszfi',Mobileclient.FROM_MAC_ADDRESS)
+if len(sys.argv) > 1:
+    pw = ""
+    for line in fileinput.input():
+        pw = line
 
-if logged_in:
-    
-    print("Getting playlists...\n")
-    all_playlists = api.get_all_user_playlist_contents()
-    print("Getting library (for track lookup)...\n")
-    my_library = api.get_all_songs()
-    
-    for playlist in all_playlists:
-        if playlist['name'] in lists_to_get:
-            print("Parsing playlist "+playlist['name']+"...\n")
-            count = 0
-            f = open(date.isoformat(date.today())+" -- "+playlist['name'],'w')
-            for i in playlist['tracks']:
-                track = i.get('track')
-                if not track:
-                    lib_track = [item for item in my_library if item.get('id') in i.get('trackId')]
-                    track = lib_track[0]
-                f.write(str(count+1)+","+track['artist']+","+track['album']+","+track['title']+"\n")
-                count+=1
-            f.close()            
-                
-api.logout()
+    api = Mobileclient()
+    logged_in = api.login('kevinecasey@gmail.com',pw,Mobileclient.FROM_MAC_ADDRESS)
+
+    if logged_in:
+        
+        print("Getting playlists...")
+        all_playlists = api.get_all_user_playlist_contents()
+        print("Getting library (for track lookup)...")
+        my_library = api.get_all_songs()
+        
+        for playlist in all_playlists:
+            if playlist['name'] in lists_to_get:
+                print("Parsing playlist "+playlist['name']+"...")
+                count = 0
+                f = open(date.isoformat(date.today())+" -- "+playlist['name'],'w')
+                for i in playlist['tracks']:
+                    track = i.get('track')
+                    if not track:
+                        lib_track = [item for item in my_library if item.get('id') in i.get('trackId')]
+                        track = lib_track[0]
+                    f.write(str(count+1)+","+track['artist']+","+track['album']+","+track['title']+"\n")
+                    count+=1
+                f.close()            
+                    
+    api.logout()
+
+else:
+    print("Please include the path to a file containing a Google Music password.")
